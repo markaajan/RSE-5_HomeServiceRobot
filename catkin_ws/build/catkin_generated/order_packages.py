c336ad10c6c76289415ea395c6c969a0894d1d6c
markose.aajan@gmail.com <markose.aajan@gmail.com>
2022-07-29 15:11:10 +0000

    Completed

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/root/temp/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/workspace/temp/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/workspace/temp/catkin_ws/devel;/opt/ros/kinetic" != "" else []
--- catkin_ws/build/turtlebot/turtlebot_teleop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/temp/catkin_ws/src/turtlebot/turtlebot_teleop
-# Build directory: /root/temp/catkin_ws/build/turtlebot/turtlebot_teleop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
